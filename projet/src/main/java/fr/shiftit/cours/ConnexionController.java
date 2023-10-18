c0571842ab2355fe68a5f0c9e5767b8a3debfa03
ClementMounic <clement.mounic65@gmail.com>
2023-10-18 12:31:16 +0200

    controller

--- projet/src/main/java/fr/shiftit/cours/ConnexionController.java
+++ projet/src/main/java/fr/shiftit/cours/ConnexionController.java
@@ -3,15 +3,22 @@ package fr.shiftit.cours;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ConnexionController {
 
 	
 	@GetMapping(path = "/connexion")
-	public String ioupi(Model model) {
+	public String connexion(Model model) {
 	
 		return "connexion";
 	}
+	
+    @PostMapping(path= "/connexion")
+    public String processLogin() {
+        // Logique d'authentification ici
+        return "redirect:/index";
+    }
 	
 }
